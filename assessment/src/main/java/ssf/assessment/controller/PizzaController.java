1b6a90bb18d6e66ed283c8713cc5bfef35f844af
ngyuqi <ngyuqi89@gmail.com>
2023-01-27 15:33:47 +0800

    Draft 3

--- assessment/src/main/java/ssf/assessment/controller/PizzaController.java
+++ assessment/src/main/java/ssf/assessment/controller/PizzaController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import jakarta.servlet.http.HttpServletResponse;
@@ -29,25 +30,60 @@ public class PizzaController {
     //Method to process pizza form and perform validation
     //Returns view 1 - custdetails
     @PostMapping(path="/pizza")
-    public String showViewOne(@Valid Order order, BindingResult bindingResult, Model model) {
-        // if(bindingResult.hasErrors()){
+    public String showViewOne(@Valid Order order, BindingResult result, Model model) {
+        // if(result.hasErrors()){
         //     return "index";
         // }
-        model.addAttribute("order", new Order());
+        model.addAttribute("order", order);
         return "custdetails";
     }
 
     //Method to process customerdetails form and perform validation
     //Returns view 2 - orderdetails
     @PostMapping(path="/pizza/order")
-    public String showViewOne(@Valid Order order, BindingResult bindingResult, Model model, HttpServletResponse response) throws IOException {
-        // if (bindingResult.hasErrors()){
+    public String showViewOne(@Valid Order order, BindingResult result, @ModelAttribute Order o, Model model, HttpServletResponse response) throws IOException {
+        // if (result.hasErrors()){
         //     return "custdetails";
         // }
+        calculateCost(model, o.getPizza(), o.getSize(), o.getQuantity());
+        //model.addAttribute( "order", order);
         response.setStatus(HttpServletResponse.SC_CREATED);
         return "orderdetails";
+    }
 
+    //Method to calculate pizza cost
+    private void calculateCost(Model model, String pizza, String size, Integer quantity){
+        
+        //Set pizza price
+        double pizzaPrice = 0f;
+        if (pizza == "bella" || pizza == "marinara" || pizza == "spianatacalabrese" ) {
+            pizzaPrice = 30.00; 
+        }
+        else if (pizza == "margherita") {
+            pizzaPrice = 22.00;
+        }
+        else if (pizza == "trioformaggio"){
+            pizzaPrice = 25.00;
+        }
+        System.out.println("Price is " + pizzaPrice);
 
+        //Set size multiplier
+        double sizeMulti = 0f;
+        if (size == "sm") {
+            sizeMulti = 1; 
+        }
+        else if (size == "md") {
+            sizeMulti = 1.2; 
+        }
+        else if (size == "lg") {
+            sizeMulti = 1.5; 
+        }
+        System.out.println("Size multiplier is " + sizeMulti);
+
+        //Total price
+        double total = pizzaPrice * sizeMulti * quantity;
+        System.out.println("Total price is $" + total);
+        model.addAttribute("total", total);
     }
 
 }
